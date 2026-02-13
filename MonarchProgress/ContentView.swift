00586598a8280e0fdbf39f7cf110066db2148b4c
Aver Alexey <68560245+cosmograve@users.noreply.github.com>
2026-02-13 13:35:53 +0700

    Initial commit

---
+++ MonarchProgress/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MonarchProgress
+//
+//  Created by Алексей Авер on 13.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MonarchProgress/MonarchProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonarchProgressApp.swift
+//  MonarchProgress
+//
+//  Created by Алексей Авер on 13.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MonarchProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
